c57d560c4b107d48090d1ad414d8b118ce5dec67
Michael Douglass <Michael>
2025-04-18 21:02:46 +0930

    updated manifest and wheels to be compatible with blender extensions platform

--- download_wheels.py
+++ download_wheels.py
@@ -3,16 +3,9 @@ import subprocess
 
 def main():
     packages = [
-        "imageio==2.36.0",
-        "lazy_loader==0.4",
-        "networkx==3.4.2",
-        "numpy==2.1.2",
-        "packaging==24.1",
-        "pillow==11.0.0",
-        "pydicom==2.3.1",
         "scikit-image==0.24.0",
-        "scipy==1.14.1",
-        "tifffile==2024.9.20"
+        "pydicom==2.3.1",
+        "scipy==1.14.1"
     ]
 
     wheels_dir = "./wheels"
